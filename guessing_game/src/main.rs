967a65d502e705bf2d56642c36ed5a7b484aafe5
Brian De Maio <bdemaio1@gmail.com>
2020-07-15 02:07:32 -0400

    Reading in user input, and comparing against a random number, and returning if the user guesses the correct secret number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,30 +5,46 @@ use std::io;
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is: {}", secret_number);
-    println!("Please input your guess.");
-    let mut guess = String::new();
-    /*Return type checked by 'expect',
-    checking for Ok or Err returned result.
-    If Ok - Value is returned
-    If Err - Error returned,
-    program stops and error message displayed*/
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    //println!("The secret number is: {}", secret_number);
+    //Infinite loop to allow multiple guesses
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        /*Return type checked by 'expect',
+        checking for Ok or Err returned result.
+        If Ok - Value is returned
+        If Err - Error returned,
+        program stops and error message displayed*/
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    /*Convert from String to numeric u32 guess.
-    Type required for parse of string*/
-    //.parse returns a 'Result' type, so 'expect' is needed
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        /*Convert from String to numeric u32 guess.
+        Type required for parse of string*/
+        //.parse returns a 'Result' type, so 'expect' is needed
+        //let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
+        /*Since above line doesn't stop invalid inputs
+        We can unpack the returned 'Result' type, check the match,
+        and return the value, or go back to the beginning of the loop
+        if there is an error, to prompt the user again*/
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    /*Sort of like a switch statement in other
-    languages*/
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("You guessed: {}", guess);
+
+        /*Sort of like a switch statement in other
+        languages*/
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                //Exit on win
+                break;
+            }
+        }
     }
 }
