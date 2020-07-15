0092095a9f884f970365c71d274f9152a631f1a8
Brian De Maio <bdemaio1@gmail.com>
2020-07-15 01:40:32 -0400

    Reading in user input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,6 +4,11 @@ fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
     let mut guess = String::new();
+    /*Return type checked by 'expect',
+    checking for Ok or Err returned result.
+    If Ok - Value is returned
+    If Err - Error returned,
+    program stops and error message displayed*/
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
