f4877974e5a2835eaea881bfc32e286be98b9093
Brian De Maio <bdemaio1@gmail.com>
2020-07-18 19:56:07 -0400

    Taking in a little struct instruction

--- strut_practice/src/main.rs
+++ strut_practice/src/main.rs
@@ -1,4 +1,6 @@
-fn main() {
+//Too simple and data not held together in an
+//informative way
+/*fn main() {
     let width1 = 30;
     let height1 = 50;
 
@@ -10,4 +12,26 @@ fn main() {
 
 fn area(width: u32, height: u32) -> u32 {
     width * height
+}*/
+
+//Adding meaning with labelled structured data
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
