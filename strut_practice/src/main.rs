c856e7545734246d9004cbcdb899744575b7f0ea
Brian De Maio <bdemaio1@gmail.com>
2020-07-18 20:19:50 -0400

    Implementing struct method on Rectangle struct, square associated function

--- strut_practice/src/main.rs
+++ strut_practice/src/main.rs
@@ -4,11 +4,27 @@ struct Rectangle {
     height: u32,
 }
 
-//Implementing a function method on the Rectangle
-//struct
+//Implementing a function method on the
+//Rectangle struct
 impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
+    }
+
+    //Can one rectangle hold another rectangle
+    //within it?
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    //Associated functions
+    //Not specified to instance of struct
+    //Returns instance of Rectangle that is a square
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
     }
 }
 
@@ -18,6 +34,11 @@ fn main() {
         height: 50,
     };
 
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
     println!(
         "The area of the rectangle is {} square pixels.",
         //Auto dereferencing works in rust
@@ -25,5 +46,8 @@ fn main() {
         rect1.area()
     );
 
-    println!("{:?}", rect1);
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+
+    println!("Associated function call {:?}", Rectangle::square(5));
+    //println!("{:?}", rect1);
 }
